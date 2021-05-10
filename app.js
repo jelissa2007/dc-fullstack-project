f295c3c299f812bc0388f8fef3180ce713e76677
myrriame <myrriametiffany@gmail.com>
2021-05-10 09:38:40 -0400

    added css and js anded routes for sign-up

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { Router } = require('express')
+// const { Router } = require('express')
 const express = require('express')
 const app = express()
 const hostname = '127.0.0.1'
@@ -7,6 +7,11 @@ const path = require('path')
 const publicDirectoryPath = path.join(__dirname, 'views')
 // const PORT = process.env.PORT || 5000
 // const Foodie = require('databasegoeshere')
+const bodyParser = require('body-parser');
+
+app.use(express.urlencoded());
+
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 app.set('views', publicDirectoryPath)
@@ -15,9 +20,13 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/foodie:id', async (req, res) => {
+app.get('/foodie/signup', (req, res) => {
     res.render('foodie/signup')
 })
+
+app.post('/foodie/signup', function (req, res, next) {
+    res.send(JSON.stringify(req.body));
+});
 
 app.get('/foodie:id', async (req, res) => {
     res.render('foodie/details')
@@ -31,9 +40,9 @@ app.get('/foodie', async (req, res) => {
 app.listen(port, hostname, () => {
     console.log(`Server is up on http://${hostname}:${port} `)
 
-    // .listen(PORT, () => console.log(`Listening on ${PORT}`))
 })
-const server = app.listen(process.env.PORT || 5000, () => {
-    const port = server.address().port;
-    console.log(`Express is working on port ${port}`);
-});
+
+// const server = app.listen(process.env.PORT || 5000, () => {
+//     const port = server.address().port;
+//     console.log(`Express is working on port ${port}`);
+// });
