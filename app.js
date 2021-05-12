dd969ffd09c99c01cc6a06f8e8a5da1f6c088248
myrriame <myrriametiffany@gmail.com>
2021-05-12 17:08:25 -0400

    pop out

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 // const { Router } = require('express')
 const express = require('express')
-const app = express()
-const hostname = '127.0.0.1'
-const port = 3005
+const bodyParser = require('body-parser');
+
 const path = require('path')
 const publicDirectoryPath = path.join(__dirname, 'views')
-const { Users } = require('./models')
-// const Foodie = require('databasegoeshere')
-const bodyParser = require('body-parser');
+
+const app = express()
+
+const PORT = process.env.PORT || 3005;
+
+// const db = require('./config/sample-config');
 
 app.use(express.urlencoded());
 
@@ -39,12 +41,12 @@ app.post('/foodie/signup', function (req, res, next) {
 //     res.render('foodie/favorites', { foodie })
 // })
 
-app.listen(port, hostname, () => {
-    console.log(`Server is up on http://${hostname}:${port} `)
+// app.listen(port, hostname, () => {
+//     console.log(`Server is up on http://${hostname}:${port} `)
 
 
-})
-const server = app.listen(process.env.PORT || 5000, () => {
-    const port = server.address().port;
-    console.log(`Express is working on port ${port}`);
+// })
+const server = app.listen(PORT, () => {
+
+    console.log(`Express is working on port ${PORT}. http://localhost:${PORT}`);
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
 alert('hi!')
+
+// popout function 
+function toggle() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+
+    let popup = document.getElementById('popup');
+    popup.classList.toggle('active')
+}
