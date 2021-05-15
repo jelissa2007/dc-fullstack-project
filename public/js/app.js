6197ecb58540259e056ccfa5cddabbc70ec9e47c
myrriame <myrriametiffany@gmail.com>
2021-05-15 10:32:49 -0400

    added like and dislike function made card centered cleaned up css

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,12 @@
-alert('hi!')
 
 // popout function 
 function toggle() {
-    let blur = document.getElementById('blur');
+    let blur=document.getElementById('blur');
     blur.classList.toggle('active')
 
-    let popup = document.getElementById('popup');
+    let popup=document.getElementById('popup');
     popup.classList.toggle('active')
 }
+
+
 
