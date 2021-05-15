8a36b11862617865e1a7cf203865cff1f3a0159f
csolis1 <celida.solis@outlook.com>
2021-05-15 10:54:32 -0400

    filters and buttons animations

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,24 @@ function toggle() {
     let popup = document.getElementById('popup');
     popup.classList.toggle('active')
 }
+
+//cuisine filter dropdown
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
