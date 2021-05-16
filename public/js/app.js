38daaaf77b73929c269f1af51e78f92287343667
Myrriame Dosso <77135051+myrriame@users.noreply.github.com>
2021-05-15 23:14:48 -0400

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,13 @@
+
 // popout function 
-function toggle() {
-  let blur = document.getElementById('blur');
-  blur.classList.toggle('active')
+// function toggle() {
+//     let blur=document.getElementById('blur');
+//     blur.classList.toggle('active')
 
-  let popup = document.getElementById('popup');
-  popup.classList.toggle('active')
+//     let popup=document.getElementById('popup');
+//     popup.classList.toggle('active')
 
-}
+// }
 
 //cuisine filter dropdown
 function myFunction() {
@@ -15,17 +16,16 @@ function myFunction() {
 
 function filterFunction() {
   var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("a");
-  for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
+  input=document.getElementById("myInput");
+  filter=input.value.toUpperCase();
+  div=document.getElementById("myDropdown");
+  a=div.getElementsByTagName("a");
+  for (i=0; i<a.length; i++) {
+    txtValue=a[i].textContent||a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter)>-1) {
+      a[i].style.display="";
     } else {
-      a[i].style.display = "none";
+      a[i].style.display="none";
     }
   }
 }
-
